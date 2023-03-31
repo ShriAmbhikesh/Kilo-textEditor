a00301684f1aba0b6fd225607b9c8ef13d7ec14b
ShriAmbhikesh <shriambhikesh@gmail.com>
2023-03-31 21:34:42 +0530

    31-3

--- kilo.c
+++ kilo.c
@@ -5,12 +5,20 @@
 #include<unistd.h>
 #include<termios.h>
 #include<stdlib.h>
+#include<sys/ioctl.h>
 
 /***defines***/
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /***data***/
-struct termios orig_termios;
+struct editorConfig{
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
+
 
 /***terminal***/
 void die(const char *s){
@@ -21,15 +29,14 @@ void die(const char *s){
 }
 
 void disableRawMode(){
-        if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)==-1) die("tcsetattr");
+        if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios)==-1) die("tcsetattr");
 }
 
-
 void enableRawMode(){
-    if(tcgetattr(STDIN_FILENO, &orig_termios)==-1) die("tcgetattr");
+    if(tcgetattr(STDIN_FILENO, &E.orig_termios)==-1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     tcgetattr(STDIN_FILENO,&raw);
     raw.c_iflag &= ~(BRKINT | ICRNL | IXON | ISTRIP | INPCK); //disables ctrl-s/q
     raw.c_oflag &= ~(OPOST);
@@ -41,7 +48,6 @@ void enableRawMode(){
 
 }
 
-
 char editorReadKey(){
     int nread;
     char c;
@@ -51,11 +57,23 @@ char editorReadKey(){
     return c;
 }
 
+int getWindowSize(int *rows, int *cols){
+    struct winsize ws;
 
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)==-1 || ws.ws_col==0){
+        if(write(STDOUT_FILENO,"\x1b[999x\x1b[999B",12)!=12) return -1;
+        editorReadKey();
+        return -1;
+    }else{
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
 /***output***/
 void editorDrawRows(){
     int y;
-    for(y=0;y<24;y++){
+    for(y=0;y<E.screenrows;y++){
         write(STDOUT_FILENO,"~\r\n",3);
     }
 }
@@ -83,9 +101,13 @@ void editorProcessKeypress(){
 
 
 /***init***/
+void initEditor(){
+    if(getWindowSize(&E.screenrows,&E.screencols)==-1) die("getWindowSize");
+}
+
 int main() {
   enableRawMode();
-  
+  initEditor();
   while (1) {
     write(STDOUT_FILENO,"\x1b[2J",4);
     write(STDOUT_FILENO,"\x1b[H",3);
